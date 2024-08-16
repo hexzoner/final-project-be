95d4ffb09631bc290464fe48837d10988c2eb191
hexzoner <hexzon@gmail.com>
2024-08-16 09:56:52 +0200

    login, me finished

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import validateJOI from "../middleware/validateJOI.js";
 import { userSchema } from "../schemas/schemas.js";
-import { signIn, signUp, me } from "../controllers/auth.js";
+import { signIn, signUp, me, getUsers } from "../controllers/auth.js";
 import verifyTokenMiddleware from "../middleware/verifyToken.js";
 
 const authRouter = Router();
 
 authRouter.route("/signup").post(validateJOI(userSchema.POST), signUp);
-authRouter.route("/signin").post(validateJOI(userSchema.LOGIN), signIn);
+authRouter.route("/login").post(validateJOI(userSchema.LOGIN), signIn);
 authRouter.route("/me").get(verifyTokenMiddleware, me);
-// authRouter.route("/users").get(verifyTokenMiddleware, getUsers);
+authRouter.route("/users").get(verifyTokenMiddleware, getUsers);
 
 export default authRouter;
