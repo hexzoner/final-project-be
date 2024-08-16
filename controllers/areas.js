1285c1b52c379527310384cf75a8f5fec3b0ab9d
hexzoner <hexzon@gmail.com>
2024-08-16 11:38:23 +0200

    CRUD for areas

--- controllers/areas.js
+++ controllers/areas.js
@@ -1,12 +1,80 @@
 import User from "../models/User.js";
-import Task from "../models/Task.js";
+import Area from "../models/Area.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
+
+export const getAllAreas = asyncHandler(async (req, res, next) => {
+  const areas = await Area.find().populate("creator", "firstName lastName email");
+  res.json(areas);
+});
 
 export const getAreas = asyncHandler(async (req, res, next) => {
   const userId = req.userId;
   const user = await User.findById(userId);
 
   if (!user) throw new ErrorResponse("User doesnt exist", 404);
-  res.json(user.areas);
+
+  const userAreas = await Area.find({ _id: user.areas });
+
+  res.json(userAreas);
+});
+
+export const createArea = asyncHandler(async (req, res, next) => {
+  const {
+    body: { name, users, status },
+  } = req;
+  const userId = req.userId;
+  const user = await User.findById(userId);
+  if (!user) throw new ErrorResponse("User doesnt exist", 404);
+
+  const area = await Area.create({ creator: userId, name, users, status });
+
+  user.areas.push(area);
+  user.save();
+
+  res.json(area);
+});
+
+export const updateArea = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id },
+    body: { name, users, status },
+  } = req;
+  const userId = req.userId;
+  const user = await User.findById(userId);
+  if (!user) throw new ErrorResponse("User doesnt exist", 404);
+
+  const area = await Area.findById(id);
+  if (!area) throw new ErrorResponse("Area doesnt exist", 404);
+
+  if (area.creator.toString() !== userId) throw new ErrorResponse("Not authorized", 401);
+
+  area.name = name;
+  area.users = users;
+  area.status = status;
+  area.save();
+
+  res.json(area);
+});
+
+export const deleteArea = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+
+  const userId = req.userId;
+  const user = await User.findById(userId);
+  if (!user) throw new ErrorResponse("User doesnt exist", 404);
+
+  const area = await Area.findById(id);
+  if (!area) throw new ErrorResponse("Area doesnt exist", 404);
+
+  if (area.creator.toString() !== userId) throw new ErrorResponse("Not authorized", 401);
+
+  await Area.findByIdAndDelete(id);
+
+  user.areas = user.areas.filter((areaId) => areaId.toString() !== id);
+  user.save();
+
+  res.json({ message: "Area deleted" });
 });
