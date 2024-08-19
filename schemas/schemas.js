11e6b10fd34b1799729e11b0af9ff13e3b19c69a
hexzoner <hexzon@gmail.com>
2024-08-19 13:50:37 +0200

    small fix to allow change password

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -5,22 +5,22 @@ export const userSchema = {
     firstName: Joi.string().optional(),
     lastName: Joi.string().optional(),
     email: Joi.string().required(),
-    password: Joi.string().alphanum().min(8).max(12).required(),
+    password: Joi.string().alphanum().min(1).max(12).required(),
     role: Joi.string().alphanum().required(),
   }),
   LOGIN: Joi.object({
     email: Joi.string().required(),
-    password: Joi.string().alphanum().min(8).max(12).required(),
+    password: Joi.string().alphanum().min(1).max(12).required(),
   }),
   PUT: Joi.object({
     firstName: Joi.string().optional(),
     lastName: Joi.string().optional(),
     email: Joi.string().optional(),
-    password: Joi.string().alphanum().min(8).max(12).optional(),
+    password: Joi.string().alphanum().min(1).max(12).optional(),
   }),
 };
 
 export const loginSchema = Joi.object({
   email: Joi.string().required(),
-  password: Joi.string().alphanum().min(8).max(12).required(),
+  password: Joi.string().alphanum().min(1).max(12).required(),
 });
