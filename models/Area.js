380895dc3664a8f958402c22d279bca7ff6047bd
hexzoner <hexzon@gmail.com>
2024-08-20 10:35:39 +0200

    updated task and area models

--- models/Area.js
+++ models/Area.js
@@ -4,6 +4,8 @@ const { Schema, model } = mongoose;
 const areaSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "User" },
   name: { type: String, required: [true, "Name is required"] },
+  address: { type: String },
+  contact: { type: String },
   users: [{ type: Schema.Types.ObjectId, ref: "User" }],
   status: { type: String, enum: ["Active", "Inactive"], default: "Active" },
   createdAt: { type: Date, default: Date.now },
