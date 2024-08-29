949a09fb80480dc25b2fbe7ec641a580187c4d2c
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-08-29 18:06:51 +0200

    Verify token Middelware added to Email.

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { staffInvitation } from "../controllers/email.js";
+import verifyTokenMiddleware from "../middleware/verifyToken.js";
+
 
 const emailRouter = Router();
 
-emailRouter.route("/invitation").post(staffInvitation);
+emailRouter.route("/invitation").post(verifyTokenMiddleware, staffInvitation);
 
 export default emailRouter;
