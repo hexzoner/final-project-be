e6b5940121f3973110b7d8ff0096e33fc885db96
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-08-29 16:40:41 +0200

    Email Services changed to nodemailer and Yahoo SMTP

--- controllers/email.js
+++ controllers/email.js
@@ -3,13 +3,16 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
 
 export const staffInvitation = asyncHandler(async (req, res, next) => {
-  const { to, subject, text } = req.body;
+  const { to, text } = req.body;
 
-  if (!to || !subject || !text) {
+  if (!to || !text) {
     return next(new ErrorResponse('Missing required fields', 400));
   }
 
-  const email = await sendEmail(to, subject, text);
+  const from = 'snaptask@outlook.com';
+  const subject = 'SnapTask Invitation';
+
+  const email = await sendEmail(from, to, subject, text);
 
   res.status(200).json({ message: 'Email sent successfully', email });
 });
--- utils/emailService.js
+++ utils/emailService.js
@@ -1,26 +1,53 @@
-import Mailgun from 'mailgun.js';
-import formData from 'form-data';
+import nodemailer from 'nodemailer';
 
-// Initialize Mailgun client
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({
-  username: 'api',
-  key: process.env.MAILGUN_API_KEY,
+const transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS, 
+  },
 });
 
-async function sendEmail(to, subject, text) {
-  try {
-    const response = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text,
-    });
-    return response;
-  } catch (error) {
-    console.error('Error sending email:', error.message);
-    throw error;
-  }
+async function sendEmail(from, to, subject, text) {
+  const mailOptions = {
+    from, 
+    to,
+    subject,
+    text,
+  };
+
+  // Send mail with defined transport object
+  const info = await transporter.sendMail(mailOptions);
+  return info;
 }
 
 export default sendEmail;
+
+// setup for using Mailgun
+
+// import Mailgun from 'mailgun.js';
+// import formData from 'form-data';
+
+// // Initialize Mailgun client
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({
+//   username: 'api',
+//   key: process.env.MAILGUN_API_KEY,
+// });
+
+// async function sendEmail(to, subject, text) {
+//   try {
+//     const response = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+//       from: process.env.EMAIL_USER,
+//       to,
+//       subject,
+//       text,
+//     });
+//     return response;
+//   } catch (error) {
+//     console.error('Error sending email:', error.message);
+//     throw error;
+//   }
+// }
+
+// export default sendEmail;
